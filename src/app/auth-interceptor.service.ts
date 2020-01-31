d7cb41102da0b345164440923a3149579037d1a4
Rafal ROjek <rafalrojek90@gmail.com>
2020-01-31 22:08:35 +0100

    Change the immutable data in the interceptor by creting a copy

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -4,6 +4,8 @@ export class AuthInterceptorService implements HttpInterceptor {
     intercept(req : HttpRequest<any>, next : HttpHandler) {
         //if (req.url) we can control the interceptor by filtering by the url 
         console.log('Request is on its way');
-        return next.handle(req); //lets the request continue to go 
+        //the original request is immutable, so we need to clone it
+        const modifiedRequest = req.clone({headers : req.headers.append('Auth', 'RafalPassword')}); //this will return a new headers object 
+        return next.handle(modifiedRequest); //lets the request continue to go 
     }
 }
